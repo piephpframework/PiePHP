545a359ed818083b0b41535ec735d6f503a85f77
Ryan <untuned20@gmail.com>
2016-03-16 23:07:24 -0500

    Console Color Support

--- App/Console/listener-example.php
+++ App/Console/listener-example.php
@@ -4,6 +4,7 @@ require_once 'autoload.php';
 
 use Application\Pie;
 use Application\Console\Console;
+use Application\Console\ConsoleMessage;
 
 $app = Pie::app();
 
@@ -11,15 +12,17 @@ $app->console(function(Console $console){
     // Create input commands to listen for
     // Commands for 'sayhi' or 'sayhello'
     $console->input('sayhi | sayhello', function(){
-        return response('Hello World');
+        return response('Hello World')->color(ConsoleMessage::Red);
     });
 
     // Quit the application when 'quit' or 'exit' is typed
     $console->input('quit|exit', function(Console $console){
         $console->quit();
-        return response('Goodbye!');
+        return response('Goodbye!')->color(ConsoleMessage::Green);
     });
 
     // Start the application
-    $console->listen("Welcome to the Matrix!\n", '> ');
+    $console->listen(function(Console $console){
+        return response('Welcome to the Matrix!')->color('green');
+    }, '> ');
 });
